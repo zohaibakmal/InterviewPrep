cfc947ab5dacb1e547d7991932428399fe13f404
zohaibakmal <zohaib.akmal@gmail.com>
2015-06-16 09:27:35 -0400

    ReverseInteger 2nd try

--- src/com/test/ReverseInteger.java
+++ src/com/test/ReverseInteger.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class ReverseInteger {
 
-	public static int reverse(int x) {
+/*	public static int reverse(int x) {
 		int reverse=0;
 		int x_copy = x;
 		int multiplier = 1;
@@ -18,11 +18,34 @@ public class ReverseInteger {
 		}
 		return reverse;
 
-	}
+	}*/
+	
+	public int reverse(int x) {
+        boolean isNegative = false;
+        if (x * (-1)>0){
+            isNegative = true;
+            x = Math.abs(x);
+        }
+        
+        int result = 0;
+        boolean first = true;
+        while (x>0){
+            if (result > Integer.MAX_VALUE/10)
+                return 0;
+            result = result*10;
+            result += x%10;
+            x= x/10;
+        }
+        if (isNegative){
+            result = result* (-1);
+        }
+        return result;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		reverse(321);
+		
+		//System.out.println(reverse(1534236469));
 	}
 
 }
