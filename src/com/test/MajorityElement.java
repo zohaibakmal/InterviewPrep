cb7a0caa3f4599de783223e0a291a17ca083d345
zohaibakmal <zohaib.akmal@gmail.com>
2015-06-16 13:11:10 -0400

    Majority Element from leetcode

---
+++ src/com/test/MajorityElement.java
@@ -0,0 +1,30 @@
+package com.test;
+
+public class MajorityElement {
+	
+	/*
+	 * 	As we sweep we maintain a pair consisting of a current candidate and a counter. Initially, the current candidate is unknown and the counter is 0.
+	 *
+	 *	When we move the pointer forward over an element e:
+	 *		If the counter is 0, we set the current candidate to e and we set the counter to 1.
+	 *		If the counter is not 0, we increment or decrement the counter according to whether e is the current candidate.
+	 *	When we are done, the current candidate is the majority element, if there is a majority.
+	 * */
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0];
+        int count = 1;
+        for (int i=1;i<nums.length;i++){
+            if (majorityElement==nums[i]){
+                count++;
+            }else{
+                if (count>0){
+                    count--;
+                }else{
+                    majorityElement=nums[i];
+                    count++;
+                }
+            }
+        }
+        return majorityElement;
+    }
+}
