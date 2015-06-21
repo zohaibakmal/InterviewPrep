39e7ddb592a2ce306c680e426aa356fcb2b31930
zohaibakmal <zohaib.akmal@gmail.com>
2015-06-21 14:57:07 -0400

    From Leetcode

---
+++ src/com/test/RectangleArea.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class RectangleArea {
+	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+		int areaOfSqrA = (C-A) * (D-B);
+		int areaOfSqrB = (G-E) * (H-F);
+
+		/*
+		 * We calculate the overlapping rectangle dimensions
+		 * 
+		 * */
+		
+		int left = Math.max(A, E);
+		int right = Math.min(G, C);
+		int bottom = Math.max(F, B);
+		int top = Math.min(D, H);
+
+		//If overlap
+		int overlap = 0;
+		if(right > left && top > bottom)
+			overlap = (right - left) * (top - bottom);
+
+		return areaOfSqrA + areaOfSqrB - overlap;
+	}
+}
